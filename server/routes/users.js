a4c58cd7b54eea4ebe88ac15ba7c31fdb14b3076
Charles Bon Chua <chuacharlesbon@gmail.com>
2022-04-28 21:18:00 +0800

    modify first route

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,9 +4,11 @@ const router = express.Router();
 
 const userControllers = require("../controllers/user-controllers")
 
+const auth = require("../auth");
 
+const {verify, verifyAdmin} = auth;
 
-
+router.post('/register', userControllers.registerUser)
 
 
 
