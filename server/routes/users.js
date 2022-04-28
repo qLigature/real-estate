ab7de18ac6790c8dc9c7f0daa817d98dd87c124a
Charles Bon Chua <chuacharlesbon@gmail.com>
2022-04-28 22:08:38 +0800

    modify module, add route

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const userControllers = require("../controllers/user-controllers")
+const userControllers = require("../controllers/user-controller")
 
 const auth = require("../auth");
 
