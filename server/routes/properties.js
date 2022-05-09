c6cfea6c11687d2fe91d976dfefeec6e769c90f6
Charles Bon Chua <chuacharlesbon@gmail.com>
2022-05-09 16:12:07 +0800

    added new routes for properties

--- server/routes/properties.js
+++ server/routes/properties.js
@@ -16,6 +16,24 @@ router.put('/editPrp/:id', prpControllers.editPrp);
 
 router.get('/getAllPrp', prpControllers.getAllPrp);
 
+router.get('/getPrpByName/:id', prpControllers.getPropertyByNameParams)
 
+router.get('/getPrpByNameBdy', prpControllers.getPropertyByNameReqBody)
+
+router.get('/getPrpById/:id', prpControllers.getPropertyById)
+
+router.get('/getCategoryHotel', prpControllers.getPrpCategoryHotel)
+
+router.get('/getCategoryTownhouse', prpControllers.getPrpCategoryTownhouse)
+
+router.get('/getCategoryCondo', prpControllers.getPrpCategoryCondominium)
+
+router.get('/getCategoryTower', prpControllers.getPrpCategoryTower)
+
+router.get('/prpCityManila', prpControllers.cityManila)
+
+router.get('/prpCityQC', prpControllers.cityQC)
+
+router.get('/prpNearMe', prpControllers.propertyNearMe)
 
 module.exports = router;
